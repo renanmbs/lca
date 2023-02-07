f5d901d43108905088691d18c7e1454dcf803942
Renan Mougenot Breviglieri Silva <mougenot.renan@gmail.com>
2023-02-06 21:33:33 -0700

    Feb 6  2023

---
+++ src/components/banner/banner.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./banner.css";
+
+export class Banner extends React.Component{
+    render(){
+        return(
+            <div id="home">
+
+                <div id="banner">
+                    <h1>LCA <br/> Men's Soccer</h1>
+                </div>
+            </div>
+        );
+    }
+}
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './navbar.css';
+import logo from "./LCA-LOGO.png";
+import { Players } from '../players/players';
+import { createRoot } from 'react-dom/client';
+
+export class NavBar extends React.Component{
+
+  hide(){
+    const root = createRoot(document.getElementById('base'));
+    root.unmount();
+  }
+
+  showPlayers(){
+    const root = createRoot(document.getElementById('base'));
+    root.render(<Players/>);
+  }
+
+  showStat(){
+    //Render stat component
+  }
+
+  showSchedule(){
+    //Render schedule component
+  }
+
+  showPhotos(){
+   //Render photos component
+  }
+
+  render(){
+    return(
+      <div>
+         <nav>
+            <ul>
+              <li><a href='https://www.maxpreps.com/ut/layton/layton-christian-academy-eagles/soccer/spring/' target="_blank" rel="noopener noreferrer"><img src={logo}/></a></li>
+              <li><a href='#home' onClick={this.hide}>Home</a></li>
+              <li><a href="#players" onClick={this.showPlayers}>Roster</a></li>
+              <li><a href="#stats" onClick={this.showStat}>Statistics</a></li>
+              <li><a href="#schedule" onClick={this.showSchedule}>Schedule</a></li>
+              <li><a href="#media" onClick={this.showPhotos}>Photos & Videos</a></li>
+            </ul>
+          </nav>
+      </div>
+    )
+  }
+}
+
+
+
